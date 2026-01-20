7b025a21436ca197ea93c3ca300b4d55a5720437
Atlee Circuitree <atleecircuitree@gmail.com>
2026-01-20 18:08:49 -0500

    Baileys code adventure

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,9 +8,28 @@ package frc.robot;
 /** Add your docs here. */
 public class Constants {
 
+    public class CAN_IDS {
+        public static final int wristMotor = 31;
+        public static final int wristEncoder = 3;
+    }
+
+    public class Wrist {
+        public static final double[] wristLimit =  new double[] {330, 30};
+        public static final double wristEncoderOffset = 0.351;
+        public static final double P = 0.1;
+        public static final double I = 0;
+        public static final double D = 0;
+    }
+
+    /*
+     * lime light values =======================================
+     * scale = 46.39986
+     * distance = (scale / ta)
+     */
     public class Properties {
 
         public static double intakeVelocity = .5;
+        
     }
   
 }
